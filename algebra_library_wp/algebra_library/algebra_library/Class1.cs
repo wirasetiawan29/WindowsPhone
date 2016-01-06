de1ddec2a2aa5b9d8ab0e384011fabf2cfd366a1
wira <wirasetiawan29@jojonomic.com>
2016-01-06 14:06:13 +0700

    Algebra
    Windows Phone Apps Sample

---
+++ algebra_library_wp/algebra_library/algebra_library/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace algebra_library
+{
+    public class Algebra
+    {
+        public double Addition(double x, double y)
+        {
+            return x + y;
+        }
+        public double Subtraction(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiplication(double x, double y)
+        {
+            return x * y;
+        }
+        public double Division(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ algebra_library_wp/algebra_test/algebra_test/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using algebra_test.Resources;
+using algebra_library;
+
+namespace algebra_test
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Algebra alg = new Algebra();
+            double number1 = 100;
+            double number2 = 50;
+            double result = alg.Addition(number1, number2);
+            hasil.Text = result.ToString("0.000000");
+        }
+
+    }
+}
