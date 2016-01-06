2de7e296baf3011236f5f06781d965d9c0e4d3f4
wira <wirasetiawan29@jojonomic.com>
2016-01-06 13:58:25 +0700

    Device Status
    Windows Phone Apps Sample

---
+++ device_status/device_status/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using device_status.Resources;
+
+namespace device_status
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ device_status/device_status/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using device_status.Resources;
+using Microsoft.Phone.Info;
+
+namespace device_status
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            DeviceManufacturer.Text = DeviceStatus.DeviceManufacturer;
+            DeviceName.Text = DeviceStatus.DeviceName;
+            FirmwareVersion.Text = DeviceStatus.DeviceFirmwareVersion;
+            HardwareVersion.Text = DeviceStatus.DeviceHardwareVersion;
+            DeviceTotalMemory.Text = DeviceStatus.DeviceTotalMemory.ToString();
+            ApplicationCurrentMemoryUsage.Text = DeviceStatus.ApplicationCurrentMemoryUsage.ToString();
+            ApplicationMemoryUsageLimit.Text = DeviceStatus.ApplicationMemoryUsageLimit.ToString();
+            ApplicationPeakMemoryUsage.Text = DeviceStatus.ApplicationPeakMemoryUsage.ToString();
+            IsKeyboardPresent.Text = DeviceStatus.IsKeyboardPresent.ToString();
+            IsKeyboardDeployed.Text = DeviceStatus.IsKeyboardDeployed.ToString();
+            PowerSource.Text = DeviceStatus.PowerSource.ToString();
+            DeviceStatus.PowerSourceChanged += new EventHandler(DeviceStatus_PowerSourceChanged);
+            DeviceStatus.KeyboardDeployedChanged += new EventHandler(DeviceStatus_KeyboardDeployedChanged);
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        void DeviceStatus_KeyboardDeployedChanged(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => IsKeyboardDeployed.Text = DeviceStatus.IsKeyboardDeployed.ToString());
+        }
+
+        void DeviceStatus_PowerSourceChanged(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => PowerSource.Text = DeviceStatus.PowerSource.ToString());
+        }
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
